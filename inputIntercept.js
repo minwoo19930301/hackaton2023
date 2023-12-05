0b2e0eb0c0dcba598f3c1f10ee5c69881a906b88
김민우 <rlaalsdn456456@naver.com>
2023-12-05 14:54:39 +0900

    [feat] 예시 더 넣고 json으로 데이터화

--- inputIntercept.js
+++ inputIntercept.js
@@ -1,9 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
+    let currentEntry = null;
+    function loadRandomEntry(data) {
+        currentEntry = data[Math.floor(Math.random() * data.length)];
+        document.querySelector('.form__input').setAttribute('placeholder', currentEntry.placeholder);
+    }
+
+    fetch('prequery.json')
+        .then(response => response.json())
+        .then(data => loadRandomEntry(data))
+        .catch(error => console.error('Error loading JSON:', error));
+
     document.querySelector('.form').addEventListener('submit', function(event) {
         var inputValue = document.querySelector('.form__input').value;
-        if (inputValue === '21' || inputValue === '21호' || inputValue === '파운데이션 21호') {
+        if (currentEntry && currentEntry.answers.includes(inputValue)) {
             event.preventDefault();
-            window.location.href = 'congrats.html';
+            window.location.href = currentEntry.destination;
         }
     });
 });
